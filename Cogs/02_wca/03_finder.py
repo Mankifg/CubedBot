0f1cbde86374e3c43fee1699cf784876ac8ba244
Mankifg <83959255+Mankifg@users.noreply.github.com>
2024-01-14 20:57:36 +0100

    finder

---
+++ Cogs/02_wca/03_finder.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+import requests, json
+
+import db
+import hardstorage
+import wca_functions
+import functions
+
+from datetime import datetime as dt
+
+meseci = [
+    "1 January",
+    "2 February",
+    "3 March",
+    "4 April",
+    "5 May",
+    "6 June",
+    "7 July",
+    "8 August",
+    "9 September",
+    "10 October",
+    "11 November",
+    "12 December",
+]
+
+
+class finderCog(commands.Cog, name="finder command"):
+    def __init__(self, bot: commands.bot):
+        self.bot = bot
+
+    @discord.command(name="finder", usage="", description="no")
+    @commands.cooldown(1, 2, commands.BucketType.member)
+    async def finder(
+        self,
+        ctx: discord.ApplicationContext,
+        dan: int,
+        mesec: discord.Option(choices=meseci),
+        leto: int,
+        drzava,
+    ):
+
+        comps = wca_functions.find_by_date(dan, mesec, leto)
+
+        await ctx.send("finder")
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(finderCog(bot))
